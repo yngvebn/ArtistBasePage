42daff38fc9c80a4c07a47cb26141c91075c0b62
unknown <yngve.nilsen@gmail.com>
2012-09-26 10:51:55 +0200

    Added some more stuff

--- Domain/Db.cs
+++ Domain/Db.cs
@@ -1,13 +1,16 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 
 namespace Domain
 {
     public class Db: DbContext
     {
+        public DbSet<Artist> Artists { get; set; }
         public DbSet<Song> Songs { get; set; }
         public DbSet<Album> Albums { get; set; }
         public DbSet<Event> Events { get; set; }
         public DbSet<Location> Locations { get; set; }
         public DbSet<GalleryImage> GalleryImages { get; set; }
+        public DbSet<Article> Articles { get; set; }
     }
 }
