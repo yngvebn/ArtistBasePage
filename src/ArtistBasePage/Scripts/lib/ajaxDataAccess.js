1fe0bc7b6d1dd1fc5c15a5da947e17be58f1b8fc
Yngve Bakken Nilsen <yngve.nilsen@gmail.com>
2012-10-01 22:53:56 +0200

    Started Admin GUI

--- src/ArtistBasePage/Scripts/lib/ajaxDataAccess.js
+++ src/ArtistBasePage/Scripts/lib/ajaxDataAccess.js
@@ -1,12 +1,14 @@
 ﻿var AjaxDataAccess = (function () {
 
-    var ajax = function (method, url, successCallback) {
+    var ajax = function (method, url, successCallback, data) {
         $.ajax({
             headers:
                 {
                     t: constants.token
                 },
             url: url,
+            type: method,
+            data: data === undefined ? null : data,
             contentType: 'application/json',
             dataType: 'json',
             success: function (response) {
@@ -19,8 +21,12 @@
     var get = function (url, success) {
         ajax('get', url, success);
     };
+    var save = function(url, data, success) {
+        ajax('put', url, success, data);
+    };
 
     return {
-        get: get
+        get: get,
+        save: save
     };
 });
